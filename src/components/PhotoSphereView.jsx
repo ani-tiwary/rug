ee2beda289222b13508380ad323d11d072573b5e
ani tiwary <137713147+ani-tiwary@users.noreply.github.com>
2025-02-01 14:40:26 -0500

    it works

--- src/components/PhotoSphereView.jsx
+++ src/components/PhotoSphereView.jsx
@@ -7,7 +7,7 @@ import PhotoSphere from './PhotoSphere';
 const PhotoSphereView = () => {
   return (
     <Canvas camera={{ position: [0, 0, 0.1] }}>
-      <PhotoSphere imageUrl="/panorama.jpg" />
+      <PhotoSphere imageUrl="/ps/ps1.jpg" />
       <OrbitControls enableZoom={false} enablePan={false} rotateSpeed={0.4} />
     </Canvas>
   );
